8ead6fbe2bc7cb119362f1c041d258b1eba15615
Polina <polina_cherkasova@outlook.com>
2024-08-01 16:24:02 +0300

    only file logging from now on; +function signature while tracing; +concurrency feature; resolved some TODO's

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,6 +1,7 @@
 #include "Logger.h"
 
 LogPriority Logger::s_priority = INFO;
+std::mutex Logger::s_log_mutex;
 
 LogPriority Logger::get_priority()
 {
@@ -26,63 +27,66 @@ Logger::Logger(const LogPriority user_priority)
 void Logger::Log(LogPriority user_priority, const std::optional<std::string>& function_name, const std::string& message,
 				const std::string& file, int line)
 {
-	std::string color{};
+	std::lock_guard<std::mutex> guard(s_log_mutex);
 	std::string output_priority{};
 
 	switch (user_priority)
 	{
 	case TRACE:
-		color = "\033[1;36m"; // cyan
 		output_priority = "TRACE";
 		break;
 	case DEBUG:
-		color = "\033[1;34m"; // blue
 		output_priority = "DEBUG";
 		break;
 	case PROD:
-		color = "\033[1;35m"; // purple
 		output_priority = "PROD";
 		break;
 	case INFO:
-		color = "\033[1;32m"; // green
 		output_priority = "INFO";
 		break;
 	case WARNING:
-		color = "\033[1;33m"; // yellow
 		output_priority = "WARNING";
 		break;
 	case ERROR:
-		color = "\033[1;31m"; // red
 		output_priority = "ERROR";
 		break;
 	default:
-		color = "\033[1;37m";
 		output_priority = "UNKNOWN";
 		break;
 	}
 
-	PrintLogging(color, output_priority, function_name, message, file, line);
+	FileLogging(output_priority, function_name, message, file, line);
 }
 
-void Logger::PrintLogging(const std::string& color, const std::string& output_priority,
+void Logger::FileLogging(const std::string& output_priority,
 						const std::optional<std::string>& function_name, const std::string& message,
 						const std::string& file, const int line)
 {
-	// TODO: convert time setting to use localtime_s for safety
 	const auto now_time_point = std::chrono::system_clock::now();
 	const std::time_t now_time = std::chrono::system_clock::to_time_t(now_time_point);
-	const std::tm now = *std::localtime(&now_time);
+	std::tm now{};
 
-	// TODO: some mode switch between file and console?
-	// console output
-	std::cout << color << std::put_time(&now, "%Y-%m-%d %H:%M:%S") << " [" << output_priority << "] ";
+#ifdef _WIN32
+	localtime_s(&now, &now_time);
+#else
+	localtime_r(&now_time, &now);
+#endif
+
+	// file output
+	std::ofstream log_file(SERVERLOG, std::ios_base::out | std::ios::app);
+	if (!log_file.is_open())
+	{
+		std::cerr << "error while opening the " << SERVERLOG << "! bad!";
+		return;
+	}
+	log_file << std::put_time(&now, "%Y-%m-%d %H:%M:%S") << " [" << output_priority << "] ";
 	if (function_name.has_value())
 	{
-		std::cout << "(" << function_name.value() << ") ";
+		log_file << "(" << function_name.value() << ") ";
 	}
 	if (!file.empty())
 	{
-		std::cout << file.substr(file.find("src")) << ":(" << line << ')' << ' ';
+		log_file << file.substr(file.find("src")) << ":(" << line << ')' << ' ';
 	}
-	std::cout << message << "\033[0m" << '\n';
+	log_file << message << '\n';
 }
--- src/Logger.h
+++ src/Logger.h
@@ -1,3 +1,12 @@
+#pragma once
+
+#ifdef _MSC_VER
+#define __FUNCTION_SIGNATURE__ __FUNCSIG__
+#else
+#define __FUNCTION_SIGNATURE__ __PRETTY_FUNCTION__
+#endif
+
+
 #ifndef LOGGER_H
 #define LOGGER_H
 
@@ -12,10 +21,9 @@
 #include <mutex>
 #include <optional>
 
-// small problem here. if LOG is called with manually set TRACE priority on logger, there will be no function name.
-// on the other hand, who on Earth would do that (aside from testing) when LOG_TRACE exists?
+// small problem here. if LOG is called with manually set TRACE priority on logger, there will be no function name
 #define LOG(logger, message) ((logger).Log((logger).get_priority(), std::nullopt, message, __FILE__, __LINE__))
-#define LOG_TRACE(logger, message) ((logger).Log(LogPriority::TRACE, __FUNCTION__, message, __FILE__, __LINE__))
+#define LOG_TRACE(logger, message) ((logger).Log(LogPriority::TRACE, __FUNCTION_SIGNATURE__, message, __FILE__, __LINE__))
 
 // TODO: set auto start/end(+restart for server) functions logging (by scope?)
 #define LOG_DEBUG(logger, message) ((logger).Log(LogPriority::DEBUG, std::nullopt, message, __FILE__, __LINE__))
@@ -38,9 +46,10 @@ enum LogPriority
 class Logger
 {
 	static LogPriority s_priority;
-	std::mutex m_log_mutex;
 
 public:
+	static std::mutex s_log_mutex;
+
 	Logger();
 	explicit Logger(LogPriority user_priority);
 	~Logger() = default;
@@ -48,7 +57,7 @@ public:
 	static void set_priority(LogPriority user_priority);
 	void Log(LogPriority user_priority, const std::optional<std::string>& function_name, const std::string& message,
 			const std::string& file, int line);
-	void PrintLogging(const std::string& color, const std::string& output_priority,
+	void FileLogging(const std::string& output_priority,
 					const std::optional<std::string>& function_name,
 					const std::string& message, const std::string& file, int line);
 };
