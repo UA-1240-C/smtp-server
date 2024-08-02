be66e154af65970432f9b9db575875e49dd4ee95
shevmee <shevtsov.mykhail@gmail.com>
2024-08-02 16:41:45 +0300

    Integrated a DB. Added a REGISTER command

--- cmake-build-debug/_deps/libpqxx-build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shevmee/CLionProjects/SoftServe/smtp-server-SCRUM-35-Error-handling/cmake-build-debug/_deps/libpqxx-src/test
-# Build directory: /home/shevmee/CLionProjects/SoftServe/smtp-server-SCRUM-35-Error-handling/cmake-build-debug/_deps/libpqxx-build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runner "/home/shevmee/CLionProjects/SoftServe/smtp-server-SCRUM-35-Error-handling/cmake-build-debug/_deps/libpqxx-build/test/runner")
-set_tests_properties(runner PROPERTIES  _BACKTRACE_TRIPLES "/home/shevmee/CLionProjects/SoftServe/smtp-server-SCRUM-35-Error-handling/cmake-build-debug/_deps/libpqxx-src/test/CMakeLists.txt;12;add_test;/home/shevmee/CLionProjects/SoftServe/smtp-server-SCRUM-35-Error-handling/cmake-build-debug/_deps/libpqxx-src/test/CMakeLists.txt;0;")
