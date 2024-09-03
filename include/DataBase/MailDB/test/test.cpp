9f28cf29f9ff497325a352552f46dee617caf614
Zikkend <denzik04@gmail.com>
2024-09-03 19:01:05 +0300

    removed reduntant code

--- include/DataBase/MailDB/test/test.cpp
+++ include/DataBase/MailDB/test/test.cpp
@@ -4,14 +4,6 @@
 #include "DatabaseFixture.h"
 #include "PgMailDBTest.h"
 
-
-
-const char*  CONNECTION_STRING = "dbname=testmaildb2 user=postgres password=password hostaddr=127.0.0.1 port=5432";
-
-
-
-
-
 int main(int argc, char **argv)
 {
  testing::InitGoogleTest(&argc, argv);
