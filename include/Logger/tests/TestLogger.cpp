ecac4dcda9fdd33cdf1ed979670b73a82ee4af16
Polina <polina_cherkasova@outlook.com>
2024-10-11 01:33:50 +0300

    test: added mocking workload to test multithreading

--- include/Logger/tests/TestLogger.cpp
+++ include/Logger/tests/TestLogger.cpp
@@ -154,6 +154,26 @@ TEST_F(LoggerTest, LogTraceColor)
 
 int main(int argc, char** argv)
 {
+	/*
+	// additional workload mock for debug purposes
+	Config::Logging cfg;
+	Logger::Setup(cfg);
+	auto thread_vector = std::vector<std::thread>();
+	for (int i = 0; i < 5; i++)
+	{
+		thread_vector.push_back(std::thread([]()
+		{
+			Logger::LogDebug("Thread debug log");
+			Logger::LogError("Thread error log");
+			Logger::LogTrace("fuck you!");
+		}));
+	}
+	for (auto& t : thread_vector)
+	{
+		if (t.joinable())
+			t.join();
+	}
+	*/
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
